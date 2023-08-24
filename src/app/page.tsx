27bd9cdea20ae6f2249698c25095027021770a3d
Ruben Freire Andrade <rubenswork20@gmail.com>
2023-08-24 03:54:33 +0100

    Now handling onChange values and updating them in Object to POST to DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useState, useEffect } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 import { invoke } from '@tauri-apps/api/tauri'
 
 import DateTimePicker from 'react-datetime-picker'
@@ -27,6 +27,29 @@ export default function Home() {
   //const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
+  const [formData, setFormData] = useState({
+    fullName: '',
+    location: '',
+    destination: '',
+    phone: '',
+    importantInfo: '',
+    //dateTime: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
   // async function greet() {
   //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   //   setGreetMsg(await invoke("trying_to_understand", { name }));
@@ -36,15 +59,18 @@ export default function Home() {
     <main>
       <h1>RedLux Booking Form</h1>
 
-      <form className="booking-info-form" onSubmit={(e) => {e.preventDefault(); invoke('trying_to_understand'); /*handleSubmit(); /*greet();*/}}>
+      {/* Might remove "preventDefault()" for easy refresh */}
+      <form className="booking-info-form" onSubmit={(e) => {invoke('trying_to_understand'); handleSubmit(e); /*e.preventDefault(); /*handleSubmit(); /*greet();*/}}>
         
         <div className="customer-info-container">
-          <input id="customer-name-input" /*onChange={(e) => setName(e.currentTarget.value)}*/ placeholder="Enter Customer Full Name..."/>
-          <input id="location-input" /*onChange={(e) => setName(e.currentTarget.value)}*/ placeholder="Enter Customer Location... ( Landmark, Postcode, Establishment )"/>
-          <input id="destination-input" /*onChange={(e) => setName(e.currentTarget.value)}*/ placeholder="Enter Customer Destination..."/>
-          <input id="phone-input" /*onChange={(e) => setName(e.currentTarget.value)}*/ placeholder="Enter Customer Phone Number..."/>
-          <textarea id="important-info-input" /*onChange={(e) => setName(e.currentTarget.value)}*/ placeholder="Enter Important Information..."/>
-          <DateTimePicker id="date-time-input" onChange={onChange} value={value}/>
+          <input id="customer-name-input" name="fullName" onChange={handleChange} placeholder="Enter Customer Full Name..."/>
+          <input id="location-input" name="location" onChange={handleChange} placeholder="Enter Customer Location... ( Landmark, Postcode, Establishment )"/>
+          <input id="destination-input" name="destination" onChange={handleChange} placeholder="Enter Customer Destination..."/>
+          <input id="phone-input" name="phone" onChange={handleChange} placeholder="Enter Customer Phone Number..."/>
+          <textarea id="important-info-input" name="importantInfo" onChange={handleChange} placeholder="Enter Important Information..."/>
+
+          {/* Hydration error for DataTimePicker */}
+          {/* <DateTimePicker id="date-time-input" onChange={onChange} value={value}/> */}
           <button id="booking-submit-button" type="submit">Submit Form</button>
         </div>
 
