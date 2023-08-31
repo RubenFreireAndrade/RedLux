712bf9a2da31d3ced715f9091aa2eda06fdd2bce
Ruben Freire Andrade <rubenswork20@gmail.com>
2023-08-31 15:11:23 +0100

    Now invoking 'formdata' to backend & created database struct

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,42 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-//use std::io;
 use std::env;
 use dotenv::dotenv;
-
-use mongodb::bson::{doc, Document};
-use mongodb::{Client, options::ClientOptions};
+use mongodb::bson::doc;
+use mongodb::Client;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 // #[tauri::command]
 // fn trying_to_understand(name: &str) -> String {
 //     format!("Hello, {}! You've been greeted from Rust!", name)
 // }
+
+struct Database {
+    //client_option: ClientOptions,
+    client: Client,
+    database_name: String,
+    collection_name: String,
+}
+
+impl Database {
+    fn get_client(&self) -> Client {
+        self.client.clone()
+    }
+
+    // fn get_client_option(&self) -> ClientOptions {
+    //     self.client_option.clone()
+    // }
+
+    fn get_database_name(&self) -> String {
+        self.database_name.clone()
+    }
+
+    fn get_collection_name(&self) -> String {
+        self.collection_name.clone()
+    }
+}
+
 
 #[tauri::command]
 fn trying_to_understand() {
@@ -25,18 +49,18 @@ fn test() {
 }
 
 #[tauri::command]
-async fn create_document(client: &Client, db_name: &str, collection_name: &str, full_name: &str) -> Result<String, String> {
+async fn create_document(client: &Client, db_name: &str, collection_name: &str, full_name: &str, location_of_collection: &str, location_of_destination: &str, phone_number: &str, important_information: &str) -> Result<String, String> {
     let db = client.database(db_name);
     let collection = db.collection(collection_name);
 
     let customer = doc! {
         "full_name": full_name,
-        "location_of_collection": "London",
-        "location_of_destination": "Gatwick",
-        "date_time_of_collection": "22/08/2023 | 22:00",
-        "date_time_of_destination": "02/10/2023 | 03:00",
-        "phone_number": "909019283192",
-        "important_information": "Call customer when close",
+        "location_of_collection": location_of_collection,
+        "location_of_destination": location_of_destination,
+        // "date_time_of_collection": "22/08/2023 | 22:00",
+        // "date_time_of_destination": "02/10/2023 | 03:00",
+        "phone_number": phone_number,
+        "important_information": important_information,
     };
 
     collection.insert_one(customer, None).await.unwrap();
@@ -44,22 +68,10 @@ async fn create_document(client: &Client, db_name: &str, collection_name: &str, 
     Ok(("create_customer function was used").to_string())
 }
 
-// async fn create_collection(client: &Client, db_name: &str, collection_name: &str) {
-//     let db = client.database(db_name);
-//     db.create_collection(collection_name, None).await;
-// }
-
-// async fn insert_document(client: &Client, db_name: &str, collection_name: &str) {
-//     let db = client.database(db_name);
-//     let collection = db.collection(collection_name);
-
-//     let customer = doc! {
-//         "name": "John",
-//         "age": 30
-//     };
-
-//     collection.insert_one(customer, None).await.unwrap();
-// }
+#[tauri::command]
+fn listen_submit(form_data: serde_json::Value) {
+    println!("incoming data: {}", form_data);
+}
 
 // fn insert_document_test() {
 //     let document = doc! {
@@ -80,21 +92,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mongo_username = env::var("MONGO_USERNAME").expect("MONGO_USERNAME was not found in .env");
     let mongo_password = env::var("MONGO_PASSWORD").expect("MONGO_PASSWORD was not found in .env");
 
-    // THIS IS USED TO CONNECT TO MONGODB
-    let client_options = ClientOptions::parse(format!("mongodb+srv://{u}:{p}@bookingform.woqmnkh.mongodb.net/", u = mongo_username, p = mongo_password)).await?;
-    let client = match Client::with_options(client_options) {
-        Ok(client) => {
-            println!("Connected to MongoDB");
-            client
-        }
-        Err(e) => {
-            println!("Error connecting to MongoDB: {}", e);
-            return Ok(());
-        }
+    let database = Database {
+        client: Client::with_uri_str(format!("mongodb+srv://{mongo_username}:{mongo_password}@bookingform.woqmnkh.mongodb.net/")).await?,
+        database_name: "redlux".to_string(),
+        collection_name: "bookings".to_string(),
     };
+
+    // THIS IS USED TO CONNECT TO MONGODB
+    // let client_options = ClientOptions::parse(format!("mongodb+srv://{mongo_username}:{mongo_password}@bookingform.woqmnkh.mongodb.net/")).await?;
+    // let client = match Client::with_options(client_options) {
+    //     Ok(client) => {
+    //         println!("Connected to MongoDB");
+    //         client
+    //     }
+    //     Err(e) => {
+    //         println!("Error connecting to MongoDB: {}", e);
+    //         return Ok(());
+    //     }
+    // };
     
-    create_document(&client, "redlux", "bookings", "GAMUUz").await?;
+    //create_document(&client, "redlux", "bookings", "Rubs").await?;
     
+
+
     // let desired_db = client.database("redlux");
     //let desired_collection = db.collection("bookings");
 
@@ -106,7 +126,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
     tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![test, trying_to_understand])
+    .invoke_handler(tauri::generate_handler![test, trying_to_understand, listen_submit])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
 
 import axiosLib, {isCancel, AxiosError} from 'axios';
+import { error } from 'console';
 
 export default function Home() {
 
@@ -28,12 +29,13 @@ export default function Home() {
   const [name, setName] = useState("");
 
   const [formData, setFormData] = useState({
-    fullName: '',
-    location: '',
-    destination: '',
-    phone: '',
-    importantInfo: '',
-    //dateTime: '',
+    full_name: '',
+    location_of_collection: '',
+    location_of_destination: '',
+    // date_time_of_collection: '',
+    // date_time_of_destination: '',
+    phone_number: '',
+    important_information: '',
   });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -44,10 +46,11 @@ export default function Home() {
     }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
     console.log(formData);
+
+    await invoke('listen_submit', { formData });
   };
 
   // Used to test how to invoke multiple commands from Rust backend.
@@ -67,14 +70,14 @@ export default function Home() {
       <h1>RedLux Booking Form</h1>
 
       {/* Might remove "preventDefault()" for easy refresh */}
-      <form className="booking-info-form" onSubmit={(e) => {invoke('trying_to_understand'); handleSubmit(e); /*e.preventDefault(); /*handleSubmit(); /*greet();*/}}>
+      <form className="booking-info-form" onSubmit={(e) => {handleSubmit(e); /*e.preventDefault(); /*handleSubmit(); /*greet();*/}}>
         
         <div className="customer-info-container">
-          <input id="customer-name-input" name="fullName" onChange={handleChange} placeholder="Enter Customer Full Name..."/>
-          <input id="location-input" name="location" onChange={handleChange} placeholder="Enter Customer Location... ( Landmark, Postcode, Establishment )"/>
-          <input id="destination-input" name="destination" onChange={handleChange} placeholder="Enter Customer Destination..."/>
-          <input id="phone-input" name="phone" onChange={handleChange} placeholder="Enter Customer Phone Number..."/>
-          <textarea id="important-info-input" name="importantInfo" onChange={handleChange} placeholder="Enter Important Information..."/>
+          <input id="customer-name-input" name="full_name" onChange={handleChange} placeholder="Enter Customer Full Name..."/>
+          <input id="location-input" name="location_of_collection" onChange={handleChange} placeholder="Enter Customer Location... ( Landmark, Postcode, Establishment )"/>
+          <input id="destination-input" name="location_of_destination" onChange={handleChange} placeholder="Enter Customer Destination..."/>
+          <input id="phone-input" name="phone_number" onChange={handleChange} placeholder="Enter Customer Phone Number..."/>
+          <textarea id="important-info-input" name="important_information" onChange={handleChange} placeholder="Enter Important Information..."/>
 
           {/* Hydration error for DataTimePicker */}
           {/* <DateTimePicker id="date-time-input" onChange={onChange} value={value}/> */}
